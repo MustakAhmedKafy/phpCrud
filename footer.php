d256d9daacd2d9e6193ef0de6ab7d1ec6fc2a1d0
Mustak Ahmed Kafy <ahmedmustak99@gmail.com>
2024-08-11 11:57:01 +0600

    modal add

---
+++ footer.php
@@ -0,0 +1,8 @@
+  <footer class="bg-light py-3 position-absolute bottom-0 w-100">
+    <h6 class="text-center">All right reserved Mustak Ahmed Kafy</h6>
+  </footer>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js"></script>
+  </body>
+
+  </html>
--- index.php
+++ index.php
@@ -1,57 +1,101 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Student List</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
-  <div class="container mt-5">
-    <div class="d-flex justify-content-between align-items-center mb-3">
-      <h2>All Students</h2>
-      <button type="button" class="btn btn-primary">Add Students</button>
+<?php include 'header.php' ?>
+
+<!-- table -->
+<div class="container mt-5">
+  <div class="d-flex justify-content-between align-items-center mb-3">
+    <h2>All Students</h2>
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+      Add Students
+    </button>
+
+    <!-- Modal -->
+    <div class="modal fade " id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <input type="text" class="form-control mb-3" placeholder="First Name">
+            <input type="text" class="form-control mb-3" placeholder="Last Name">
+            <input type="number" class="form-control mb-3" placeholder="Age">
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Understood</button>
+          </div>
+        </div>
+      </div>
     </div>
-    <table class="table table-bordered">
-      <thead class="thead-light">
-        <tr>
-          <th>ID</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Age</th>
-          <th>Update</th>
-          <th>Delete</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Anirban</td>
-          <td>Bhowmick</td>
-          <td>28</td>
-          <td><button class="btn btn-success">Update</button></td>
-          <td><button class="btn btn-danger">Delete</button></td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Tarun</td>
-          <td>Singh</td>
-          <td>24</td>
-          <td><button class="btn btn-success">Update</button></td>
-          <td><button class="btn btn-danger">Delete</button></td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td>Ajay</td>
-          <td>Singh</td>
-          <td>25</td>
-          <td><button class="btn btn-success">Update</button></td>
-          <td><button class="btn btn-danger">Delete</button></td>
-        </tr>
-      </tbody>
-    </table>
   </div>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js"></script>
-</body>
-</html>
+  <table class="table table-bordered">
+    <thead class="thead-light">
+      <tr>
+        <th>ID</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Age</th>
+        <th>Update</th>
+        <th>Delete</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>Anirban</td>
+        <td>Bhowmick</td>
+        <td>28</td>
+        <td>
+          <!-- Button trigger modal -->
+          <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+            Update
+          </button>
+
+          <!-- Modal -->
+          <div class="modal fade " id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  <input type="text" class="form-control mb-3" placeholder="First Name">
+                  <input type="text" class="form-control mb-3" placeholder="Last Name">
+                  <input type="number" class="form-control mb-3" placeholder="Age">
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-primary">Understood</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </td>
+        <td><button class="btn btn-danger">Delete</button></td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Tarun</td>
+        <td>Singh</td>
+        <td>24</td>
+        <td><button class="btn btn-success">Update</button></td>
+        <td><button class="btn btn-danger">Delete</button></td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>Ajay</td>
+        <td>Singh</td>
+        <td>25</td>
+        <td><button class="btn btn-success">Update</button></td>
+        <td><button class="btn btn-danger">Delete</button></td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+
+<?php include 'footer.php' ?>
